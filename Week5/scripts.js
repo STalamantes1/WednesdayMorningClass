a07aba71942792c9301698508b9605fb040231cb
STalamantes1 <STalamantes1@csi.edu>
2026-09-23 11:18:58 -0600

    Refactor HTML and JavaScript files for Week 5 and Week 6: remove inline scripts, add defer attribute, and enhance interactivity with prompts and user input.

--- Week5/scripts.js
+++ Week5/scripts.js
@@ -1,2 +1,27 @@
-let x=5
-const y=10.25
+//  //These are comments in javascript 
+//        //If we want output, we will use console.log()
+//        console.log("This is going to appear on my console");
+//        //2 Types of variables in JS. Let & Const 
+//        //let: reassignable
+//        //const: permanent
+//        let name="Sebastian"; //string text
+//        let age=30; //number/integer
+//        const isInstructor=true; //boolean 
+//        console.log(name, age, isInstructor);
+
+//        console.log(5+9+9+4+87);
+
+
+//        let first="Hello";
+//        let second="World!";
+//        console.log(first + " "+ second);
+        
+//        document.body.innerHTML += "<p>Result: " + name + "</p>";
+// let price = 10;
+// let quantity = 3;
+// let total = price * quantity;
+
+// document.body.innerHTML += "<p>Result: " + total + "</p>";
+
+let name=prompt("Enter your name");
+document.body.innerHTML += "<p>Hello! " + name + "</p>";
--- Week6/scripts.js
+++ Week6/scripts.js
@@ -1,3 +1,70 @@
-console.log("Hello")
-let username="Sebastian"
-document.body.innerHTML += "Hello " + username + "</p>";
+//comparison operators 
+//=== equal to
+// !== not equal 
+
+// console.log(1===1);
+// console.log(1!==1);
+
+// let score=85;
+// if (score>89){
+//     console.log("You got an A");
+// }
+// else if(score>79){
+//     console.log("You have a B ");
+// }
+
+// else if(score>69){
+//     console.log("You got a C")
+// }
+
+// else{
+//     console.log("You have a mediorce grade");
+// }
+
+
+
+// let age=18;
+// let isMemeber=false;
+// if (age>=18){
+//     if(isMemeber){
+//         console.log("Adult memeber benefits applied");
+//     }
+//     else{
+//         console.log("Adult, but no memebership")
+//     }
+// }else{
+//     console.log("Minor")
+// }
+
+
+
+
+
+// We use Prompt() to make our websites more interactive 
+// let score=85; 
+//this variable is hardcoded, user cant change this 
+let score=Number(prompt("Enter your score (0-100")); 
+//input is a string data type, we must convert it to the number data type 
+if (score >= 90) {
+console.log("A");
+} else if (score >= 80) {
+console.log("B");
+} else if (score >= 70) {
+console.log("C");
+} else {
+console.log("F");
+}
+
+let text = "Hello World!";
+let result = text.toUpperCase();
+
+
+// document.body.innerHTML += "<p>The score you have is a " + score + "</p>";
+//I want the website to ask the user for their name, and then display "Hello [name]"
+let username=prompt("What is your name? ");
+document.body.innerHTML += "<h1>Hello " + username.toUpperCase() + "</h1>";
+
+
+
+
+!==
